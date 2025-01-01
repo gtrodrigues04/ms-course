f6021d6e97da619cc46388e1353d84176ebcf134
gtrodrigues04 <grodrigues813@gmail.com>
2025-01-01 11:40:09 -0300

    Login jwt token

--- hr-oauth/src/main/java/com/devsuperior/hr_oauth/resources/UserResource.java
+++ hr-oauth/src/main/java/com/devsuperior/hr_oauth/resources/UserResource.java
@@ -4,20 +4,21 @@ import com.devsuperior.hr_oauth.entities.User;
 import com.devsuperior.hr_oauth.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/oauth")
 public class UserResource {
 
     @Autowired
     private UserService service;
 
     @GetMapping("/search")
-    public ResponseEntity<User> findByEmai(@RequestParam String email) {
-        return ResponseEntity.ok(service.findByEmail(email));
+    public ResponseEntity<UserDetails> findByEmail(@RequestParam String email) {
+        return ResponseEntity.ok(service.loadUserByUsername(email));
     }
 }
--- hr-oauth/src/main/java/com/devsuperior/hr_oauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hr_oauth/services/UserService.java
@@ -3,15 +3,23 @@ package com.devsuperior.hr_oauth.services;
 import com.devsuperior.hr_oauth.entities.User;
 import com.devsuperior.hr_oauth.feingclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
-    public User findByEmail(String email) {
-        return userFeignClient.findByEmail(email).getBody();
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+        return user;
     }
 }
